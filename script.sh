f28f2689f68cf2e3b8531e15f22b05bc7072a4aa
chisom <xtermcopy@gmail.com>
2025-02-11 13:10:08 -0500

    my second time. not clear though if this command applies to all files in the directory or just to the one. Will confirm

---
+++ script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo yum update -y
+sudo yum upgrade -y
+sudo yum install httpd zip -y
+sudo systemctl start httpd
+cd /tmp
+wget "https://www.tooplate.com/download/2135_mini_finance.zip"
+unzip 2135_mini-finance
+cp ./* /var/www/html/
+echo "Hello world from $(hostname -f)" > /var/www/html/index.html
+
+
